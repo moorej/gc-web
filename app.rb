a0c69512f1a83850ce0ed67b0401d8dbe63edfb9
Josh Moore <mooreja557@me.com>
2018-10-21 19:56:12 -0700

    added garden goal chart

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'date'
 require './chart'
 
 include Magick
@@ -13,11 +14,24 @@ class App < Sinatra::Application
 
     get '/spade_countdown_chart/:date' do 
         date = query_date_to_date(params[:date])
-        s = Spade.new(date)
-        
+        s = Spade.new(date)        
         t = s.next_name ? "#{s.days_until} Days Until #{s.next_name}" : "#{s.days_down} Days With #{s.name}"
-
         c = Chart.new(s.days_down, s.days_until, t)
+
+        content_type 'image/png'
+        c.to_blob
+    end
+
+    get '/garden_goal/:start_date/:end_date' do
+        start_date = query_date_to_date(params[:start_date])
+        end_date = query_date_to_date(params[:end_date])
+        today = Date.today
+
+        days_down = (today - start_date).to_i
+        days_left = today <= end_date ? (end_date - today).to_i : -1
+        t = days_left > 0 ?  "#{days_left} Days Until My Garden Goal" : "#{days_down} Days In The Garden"
+
+        c = Chart.new(days_down, days_left, t)
 
         content_type 'image/png'
         c.to_blob
